a2afab8fead18375be2ba7d3aab80fd1b7127a29
guohaoyu110 <guohaoyu110@gmail.com>
2021-12-03 04:15:41 +0800

    update main index.js

--- server/index.js
+++ server/index.js
@@ -1,41 +1,40 @@
-'use strict'; 
+require('dotenv').config(); // process.env 文件现在可以具有在.env文件中定义的键和值。
+const express = require('express');
+const morgan = require('morgan');// morgan 模块可以在command方便的显示客户端详细的请求。
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const express = require('express'); 
-const mongoose = require('mongoose'); 
-const cors = require('cors'); 
-// const errorHandler = require('./middlewares/errorHandler'); 
-const routers = require('./routers/index'); 
-const app = express(); 
-const bodyParser = require('body-parser'); 
-const PORT = process.env.PORT || 3000; 
+const app = express();
 
-const DB_URL = 'mongodb://localhost:27017/user' ;
+app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// node.js 使用mongoose操纵MongoDB
-// 使用mongoose，需要为每种类型的数据定义Schema和Model。Schema定义了一个数据实体中应该存在每个属性的数据类型。
-// MongoDB本身是没有对存入数据类型限定的机制的，而mongoose的Schema中的这种机制，形成了类似于Mysql中定义数据时的类型限定，
-// 保证了在MongoDB中存入的数据的规范性。
+// require('./routes/index.js')(app); // 必须在主文件中引入route
+// 前面是路径代表的文件，后面是app就是传入的实参，相当于express() 方法创建的web服务器。
+// app.use('./routes');
 
-mongoose.connect(DB_URL , {useNewUrlParser: true, useUnifiedTopology: true})
-    .then( () => {
-        console.log("Successfully connected！");
-    })
-    .catch(err => {
-        console.log(err); 
-        console.log("Not connected successfully!");
-    })
+// app.use('./routes/index.js'); 
+require('./routes/index.js')(app); // 必须在主文件中引入route
+// const app = require("./app");
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 8080; 
+const DB_URL = process.env.DATABASE_URL || 'mongodb://localhost/stackoverflow-clone'; 
+// 第一个内容是写在stackoverflow-clone里面的
 
-app.use(express.json()); 
-// express.json() 解析JSON格式的请求体数据，
-app.use(express.urlencoded({extended: false}));  // 解析 URL-encoded 格式的请求体数据（有兼容性，仅在 4.16.0+ 版本中可用）
-// 可以通过body-parser 对象创建中间件，当接收到客户端请求时所有的中间件都会给req.body 添加属性，请求体为空，则解析为空{} （或者出现错误）。
-app.use(cors());  // CORS跨域资源共享， 为了让你的服务器可以被其他来源来访问。
-app.use('/', routers); 
-
+mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true} )
+  .then( () => {
+    console.log("Successfully connected！");
+  })
+  .catch( err => {
+    console.log(err); 
+    console.log("Not connected successfully!");
+  })
 
 app.listen(PORT, () => {
-    console.log(`app is listening on ${PORT}`); 
+    console.log(`Server is listening on port ${PORT}`); 
 })
 
 
-module.exports = app;  
+module.exports = app;
